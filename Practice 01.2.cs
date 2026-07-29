6f032a67f72848487f3c551c45b707a1a1bb2874
UnknownWebCoder <christianjamesjagmis405@gmail.com>
2026-07-30 01:48:54 +0800

    Create Practice 01.2.cs

---
+++ Practice 01.2.cs
@@ -0,0 +1,21 @@
+using System
+
+class Program
+{
+    static void Main()
+    {
+        int number;
+
+        console.Write("Enter a number: ");
+        number = Convert.ToInt32(console.ReadLine());
+
+        if (number > 0)
+        {
+            console.WriteLibe("The number " + number + " is positive.");
+        }
+        else
+        {
+          console.WriteLine("The number " + number + " is negative.");
+        }
+    }
+}
