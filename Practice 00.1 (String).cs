676168d794b5e6b9d985d6fc35dc3bc75b971a52
UnknownWebCoder <christianjamesjagmis405@gmail.com>
2026-07-30 01:25:03 +0800

    Create Practice 00.1 (String).cs

---
+++ Practice 00.1 (String).cs
@@ -0,0 +1,17 @@
+using System
+
+class Program
+{
+    static void Main()
+    {
+        console.Write("Enter your first name: ");
+        string firstName = console.ReadLine();
+
+        console.Write("Enter your last name: ");
+        string lastName = console.ReadLine();
+
+        string fullName = firstName + " " + lastName;
+
+        console.WriteLine("Full Name: " + fullName);
+    }
+}
